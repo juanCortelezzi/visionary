8867bd6649c7a01e48cdf0ecfc5c4c43fd5efbbf
J.B.C <juanbicortelezzi@gmail.com>
2024-07-02 21:55:21 +1200

    batman

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <main>
+      <div class="text-xl text-red-200 font-bold">
+      hello there
+      </div>
+    </main>
+  );
+};
+
+export default App;
